862a354c07fec7706dc5ed3c53ea6c207f1c3b2d
Krzewskimichal <koksawp@o2.pl>
2020-10-16 14:20:14 +0200

    start api app

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import Product
+from .models import Product, Categories, Subcategories
 
 
 class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
+
+
+class CategoriesSerializer(ModelSerializer):
+    class Meta:
+        model = Categories
+        fields = '__all__'
+
+
+class SubCategoriesSerializer(ModelSerializer):
+    class Meta:
+        model = Subcategories
         fields = '__all__'
