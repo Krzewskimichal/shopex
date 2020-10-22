0dff63df468345e909523ccdb645da2c66558d56
Krzewskimichal <koksawp@o2.pl>
2020-10-22 14:54:16 +0200

    rewrite product_detail function

--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.api_overview, name='api_overview'),
     path('product-list/', views.product_list, name='product_list'),
     path('product-detail/<str:pk>/', views.product_detail, name='product_detail'),
+    path('create-product/', views.create_product, name='create_product'),
 ]
--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, get_list_or_404
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 from shop.models import Product
 from shop.serializers import ProductSerializer
@@ -12,7 +13,8 @@ from shop.serializers import ProductSerializer
 def api_overview(request):
     api_urls = {
         "Product List": "/product-list/",
-        "Product detail": "/product-detail/<str:pk>/"
+        "Product detail": "/product-detail/<str:pk>/",
+        "Create product": 'create-product/',
     }
     return Response(api_urls)
 
@@ -26,6 +28,31 @@ def product_list(request):
 
 @api_view(['GET'])
 def product_detail(request, pk):
-    product = get_object_or_404(Product, id=pk)
-    serializer = ProductSerializer(product, many=False)
+
+    try:
+        product = get_object_or_404(Product, id=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = ProductSerializer(product, many=False)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['POST'])
+def create_product(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
     return Response(serializer.data)
