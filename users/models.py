6aaed7c2769814a0653c8b98b6c079e37b2af489
Krzewskimichal <koksawp@o2.pl>
2020-10-31 16:43:21 +0100

    add profile image model

--- users/models.py
+++ users/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from shopex.settings import STATIC_URL
+
+
+def upload_profile_image_path(instance, filename):
+    return f'user_{instance.user.id}/profile_images/{filename}'
+
+
+class ProfileImage(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    upload = models.ImageField(upload_to=upload_profile_image_path)
+    upload_at = models.DateTimeField(auto_now_add=True)
