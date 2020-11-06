ff7497eb9a6ec2387f9d3453835cb7fc5d15b540
Krzewskimichal <koksawp@o2.pl>
2020-11-06 01:23:29 +0100

    create product list view

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 
-from .views import Index
+from .views import Index, ProductList
 
 app_name = 'shop'
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('/<str:gender>/', ProductList.as_view(), name='product_list'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views import View
+
+from .models import Product
 
 
 class Index(TemplateView):
     template_name = 'shop/index.html'
+
+
+class ProductList(View):
+
+    def get(self, request, gender):
+        products = Product.objects.filter(gender=gender)
+        context = {'products': products, 'gender': gender}
+        return render(request, 'shop/product_list.html', context)
